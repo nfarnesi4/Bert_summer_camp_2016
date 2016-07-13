9e85e6c2da003c741deb7e4cc6265912ab0a7bb4
Reno <nfarnesi4@gmail.com>
2016-07-13 00:43:22 -0400

    added line following mode.

--- SummerCamp2016Main/SummerCamp2016Main.ino
+++ SummerCamp2016Main/SummerCamp2016Main.ino
@@ -1,21 +1,52 @@
 #include <buttons.h>
 
 #include <RedBot.h>
-#include "buttons.h"
+
+enum RobotMode {
+  RC_MODE,
+  LINE_FOLLOWING,
+};
+
+RobotMode current_robot_mode = RC_MODE;
+
 RedBotMotors motors;
 
+//vars for line following
+RedBotSensor left = RedBotSensor(A3);   // initialize a left sensor object on A3
+RedBotSensor center = RedBotSensor(A6); // initialize a center sensor object on A6
+RedBotSensor right = RedBotSensor(A7);  // initialize a right sensor object on A7
+const int KIRSensorThreshold = 800;
+const int KForwardSpeed = 100;
+const int KTurnSpeed = 200;
+
+//vars for rc mode
 int button_id = 0;
 int button_value = 0;
 int previous_button_value = 0;
 int left_speed = 0;
 int right_speed = 0;
 
+
 void setup() {
   Serial.begin(9600);
-  
 }
 
 void loop() { 
+
+  rcDriveUpdate();
+
+  if (current_robot_mode == LINE_FOLLOWING) {
+    lineFollow();
+  }
+
+  //set the motor speeds
+  motors.leftMotor(left_speed);
+  motors.rightMotor(right_speed);
+
+}
+
+void rcDriveUpdate(){
+
   //check to see if a button was pressed or let go
   if (previous_button_value != button_value) {
     //if the button is pressed (it has a value of 1)
@@ -27,7 +58,7 @@ void loop() {
       switch (button_id) {
 
         //set the speeds of the motors when each of the buttons is pressed
-        
+
         case FORWARD_ARROW:
           left_speed = 255;
           right_speed = 255;
@@ -64,28 +95,50 @@ void loop() {
           break;
 
         case BUTTON_FOUR:
-          left_speed = 20;
-          right_speed = 20;
+          switch(current_robot_mode) {
+            case LINE_FOLLOWING:
+              current_robot_mode = RC_MODE;
+              break;
+            case RC_MODE:
+              current_robot_mode = LINE_FOLLOWING;
+              break;
+          } //swtich robot mode
           break;
 
-      }
+      }//swtich button id 
+
     }
     //when the button value is a zero 
     //set the motors to a zero
     else {
-      left_speed = right_speed = 0;
+      if (current_robot_mode == RC_MODE) {
+        left_speed = right_speed = 0;
+      }
     }
-    
+
     previous_button_value = button_value;
 
   }
+}
 
-  //set the motor speeds
-  motors.leftMotor(left_speed);
-  motors.rightMotor(right_speed);
-  
-}//delay(20);
-
+void lineFollow() {
+  if (center.read() > KIRSensorThreshold) {
+    left_speed = KForwardSpeed;
+    right_speed = KForwardSpeed;
+  }
+  else if (right.read() > KIRSensorThreshold) {
+    left_speed = KTurnSpeed;
+    right_speed = -KTurnSpeed;
+  }
+  else if (left.read() > KIRSensorThreshold) {
+    left_speed = -KTurnSpeed;
+    right_speed = KTurnSpeed;
+  }
+  else {
+    left_speed = 0;
+    right_speed = 0;
+  }
+}
 
 //handles coms with the bluetooth module
 void serialEvent() {
